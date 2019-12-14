6f68e97d14ef1327edcb4c89282a20622cf0a113
ManelMartinezDiaz <manelmdiaz@uoc.edu>
2019-12-14 20:45:41 +0100

    init commit

--- wearhealth-poc/server.js
+++ wearhealth-poc/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || 5000;
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
 });
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000);
