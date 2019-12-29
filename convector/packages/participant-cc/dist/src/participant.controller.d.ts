f92d9d4b637c5fc2e55d3a03968864efc9c37876
ManelMartinezDiaz <manelmdiaz@uoc.edu>
2019-12-29 23:56:15 +0100

    commit

--- convector/packages/participant-cc/dist/src/participant.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConvectorController } from "@worldsibu/convector-core";
-import { Participant } from "./participant.model";
-export declare class ParticipantController extends ConvectorController {
-    getParticipantById(id: string): Promise<Participant>;
-    register(id: string, name: string): Promise<void>;
-    changeIdentity(id: string, newIdentity: string): Promise<void>;
-}
--- convector/packages/participant-cc/dist/src/participant.model.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConvectorModel } from "@worldsibu/convector-core-model";
-export declare class Participant extends ConvectorModel<Participant> {
-    readonly type: string;
-    name: string;
-    msp: string;
-    x509Fingerprint: any;
-}
