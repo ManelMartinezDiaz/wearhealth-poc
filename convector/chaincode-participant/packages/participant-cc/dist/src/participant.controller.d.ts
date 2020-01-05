532392ca7410c2fa9e87aabbd9b9c2f1f99fd630
ManelMartinezDiaz <manelmdiaz@uoc.edu>
2020-01-05 06:01:03 +0100

    test ok

--- convector/chaincode-participant/packages/participant-cc/dist/src/participant.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConvectorController } from '@worldsibu/convector-core';
-import { ChaincodeTx } from '@worldsibu/convector-platform-fabric';
-import { Participant } from './participant.model';
-export declare class ParticipantController extends ConvectorController<ChaincodeTx> {
-    register(id: string, name: string): Promise<void>;
-    getParticipantById(id: string): Promise<Participant>;
-    changeIdentity(id: string, newIdentity: string): Promise<void>;
-    get(id: string): Promise<Participant>;
-}
