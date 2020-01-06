a8444c045b0996fd231c8604a3a6608f41e995d0
ManelMartinezDiaz <manelmdiaz@uoc.edu>
2020-01-06 02:25:24 +0100

    commit

--- server/dist/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var participant_controller_1 = require("./participant.controller");
-exports.ParticipantExpressController = participant_controller_1.ParticipantExpressController;
-var person_controller_1 = require("./person.controller");
-exports.PersonExpressController = person_controller_1.PersonExpressController;
-//# sourceMappingURL=index.js.map
